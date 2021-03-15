c40d9df892128e49485933ac21e2813a6933975f
Marcelo Viana <marceloviana00@gmail.com>
2021-03-15 20:20:27 -0300

    tarea

--- clase1/tarea.js
+++ clase1/tarea.js
@@ -7,18 +7,35 @@ const asyncForLoop = require('../../lovewoman/clase1/lib/asyncforloop')
 const _ = require('underscore')
 const fs = require('fs')
 
-const testFolder = 'var';
+const dirPath = 'var';
+const currentDirPath = process.mainModule.path;
+const pathSeparator = "\\";
 
-const writeDocs = () => {
+const writeDocs = async (onFinish) => {
 
-    console.log(genUsuario())
+    // console.log(genUsuario())
+    let isDir = await fs.existsSync(currentDirPath + pathSeparator + dirPath);
+    // const isDir = lstatSync.isDirectory() ;
+    // console.log({ isDir })
+    
+    if (!isDir) {
+        await fs.mkdirSync(dirPath);
+    }
+
+    isDir = await fs.existsSync(currentDirPath + pathSeparator + dirPath);
+
+    if (!isDir) {
+        console.log("Directory doesn't exists!")
+        return
+    }
+
     asyncForLoop(100, (idx, next) => {
         let obj = genUsuario()
         let usuJson = JSON.stringify(obj)
-        let pathArch = testFolder + '/' + obj.id + '.json'
+        let pathArch = dirPath + '/' + obj.id + '.json'
         fs.writeFile(pathArch, usuJson, 'utf8', (err) => {
             if (err) {
-                console.log(err)
+                console.log('asyncForLoop', { err })
                 return 
             }
             next()
@@ -26,16 +43,16 @@ const writeDocs = () => {
     }, 
     ()=>{
         console.log('finalizo el proceso')
-        exit()
+        onFinish ? onFinish() : exit()
     })
 }
 
 const readDocs = () => {
 
-    fs.readdir(testFolder, (err, files) => {
+    fs.readdir(dirPath, (err, files) => {
         files.forEach(file => {
           console.log(file);
-          fs.readFile(testFolder + '/' + file, 'utf8', function (err,data) {
+          fs.readFile(dirPath + '/' + file, 'utf8', function (err,data) {
             if (err) {
               return console.log(err);
             }
@@ -49,8 +66,8 @@ const readDocs = () => {
     
 }
 
- writeDocs()
- readDocs()
+ writeDocs(readDocs)
+//  readDocs()
 
 // console.log({env: process.env})
 // console.log({mainModule: process.mainModule.path })
