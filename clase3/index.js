4d97ed4ed060f9ea847d608f327d772cb2f45b91
Marcelo Viana <marceloviana00@gmail.com>
2021-03-29 20:36:05 -0300

    clase3

---
+++ clase3/index.js
@@ -0,0 +1,39 @@
+const obj = {
+    arrow: (paso)=>{
+        console.log(paso, Object.keys(this) );
+    },
+    log2: function(paso){
+        console.log(paso, Object.keys(this) );
+    },
+    paso1: function(){
+        this.arrow('paso1');
+        return obj
+    },
+    paso2: function(){
+        this.log2('paso2');
+        return obj
+    },
+    paso3: function(){
+        this.arrow('paso3');
+        return obj
+    },
+    paso4: function(){
+        this.log2('paso4');
+        return obj
+    },
+}
+
+// obj
+// .paso1()
+// .paso2()
+// .paso3()
+// .paso4()
+// .paso3()
+// .paso2()
+// .paso1()
+
+const promesa = (a,b) =>new Promise((resolve,reject)=>{
+    setTimeout(resolve, 2000, a+b);
+})
+
+promesa(1,2).then(result=>console.log('result',result))
