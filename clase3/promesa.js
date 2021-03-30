2be8dfa234da2b28aa1836a369a2ae3323095625
Marcelo Viana <marceloviana00@gmail.com>
2021-03-29 21:05:56 -0300

    clase3

---
+++ clase3/promesa.js
@@ -0,0 +1,11 @@
+const promesa = (a,b) =>new Promise((resolve,reject)=>{
+    if (a+b!==5) {
+        reject('Ocurrio un error')
+        return
+    }
+    setTimeout(resolve, 3000, a+b);
+})
+
+promesa(2,3)
+.then(r=>console.log(`el resultado es: ${r}`))
+.catch(err=>console.log(`Ocurrió un error: ${err}`))
